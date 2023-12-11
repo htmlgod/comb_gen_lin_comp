cf40f3b039fee5d841f05eb1de7fc0595451e444
htmlgod <ivannevtrilov212@gmail.com>
2023-12-11 15:09:20 +0300

    tests refactor, openmp algo, openmp in cmake

--- comb_gen_lin_comp/comb_gen_lin_comp.cpp
+++ comb_gen_lin_comp/comb_gen_lin_comp.cpp
@@ -5,7 +5,8 @@
 #include <random>
 #include <thread>
 #include <mutex>
-#include <iostream>
+
+#include <omp.h>
 
 #include <boost/dynamic_bitset.hpp>
 
@@ -39,38 +40,52 @@ namespace {
     size_t get_linear_complexity_bm(const std::string& gamma) {
         return 0;
     }
-    size_t get_linear_complexity_for_comb_gen_nonpar(const std::vector<std::string>& regs_gamma,
-                                                     const std::function<size_t(const std::vector<size_t>&)>& func) {
+    size_t get_linear_complexity_for_comb_gen_seq(const std::vector<std::string>& regs_gamma,
+                                                  const std::function<size_t(const std::vector<size_t>&)>& func) {
         std::vector<size_t> reg_comps;
         reg_comps.reserve(regs_gamma.size());
         for (const auto& gamma : regs_gamma)
             reg_comps.push_back(lfsrcrypto::get_linear_complexity(gamma));
         return func(reg_comps);
     }
-    size_t get_linear_complexity_for_comb_gen_par(const std::vector<std::string>& regs_gamma,
-                                                  const std::function<size_t(const std::vector<size_t>&)>& func) {
+    size_t get_linear_complexity_for_comb_gen_openmp(const std::vector<std::string>& regs_gamma,
+                                                     const std::function<size_t(const std::vector<size_t>&)>& func) {
+        std::vector<size_t> reg_comps;
+        reg_comps.resize(regs_gamma.size());
+        #pragma omp parallel num_threads(regs_gamma.size()) shared(regs_gamma, reg_comps)
+        {
+            int i = omp_get_thread_num();
+            reg_comps[i] = lfsrcrypto::get_linear_complexity(regs_gamma[i]);
+        }
+        return func(reg_comps);
+    }
+    size_t get_linear_complexity_for_comb_gen_threads(const std::vector<std::string>& regs_gamma,
+                                                      const std::function<size_t(const std::vector<size_t>&)>& func) {
+        std::vector<size_t> reg_comps;
+        std::vector<std::thread> threads;
+        std::mutex m1;
+        for (const auto& gamma : regs_gamma)
+            threads.emplace_back(std::thread([&m1, &reg_comps, &gamma](){
+                auto res = lfsrcrypto::get_linear_complexity(gamma);
+                std::lock_guard<std::mutex> l(m1);
+                reg_comps.push_back(res);
+            }));
+        for (auto&& t : threads)
+            t.join();
+        return func(reg_comps);
+    }
+    size_t get_linear_complexity_for_comb_gen_async(const std::vector<std::string>& regs_gamma,
+                                                    const std::function<size_t(const std::vector<size_t>&)>& func) {
         std::vector<std::future<size_t>> futures;
-        //futures.reserve(regs_gamma.size());
+        futures.reserve(regs_gamma.size());
         for (auto& gamma : regs_gamma)
-            futures.push_back(std::async(std::launch::async, &lfsrcrypto::get_linear_complexity, std::move(gamma)));
+            futures.push_back(
+                    std::async(std::launch::async, &lfsrcrypto::get_linear_complexity, std::move(gamma)));
 
         std::vector<size_t> reg_comps;
-        //reg_comps.reserve(regs_gamma.size());
+        reg_comps.reserve(regs_gamma.size());
         for (auto&& future : futures)
             reg_comps.push_back(future.get());
-
-        //std::vector<size_t> reg_comps;
-        //std::vector<std::thread> threads;
-        //std::mutex m1;
-        //for (const auto& gamma : regs_gamma)
-        //    threads.emplace_back(std::thread([&m1, &reg_comps, &gamma](){
-        //        auto res = lfsrcrypto::get_linear_complexity(gamma);
-        //        std::lock_guard<std::mutex> l(m1);
-        //        reg_comps.push_back(res);
-        //    }));
-        //for (auto&& t : threads)
-        //    t.join();
-
         return func(reg_comps);
     }
 }
@@ -84,25 +99,36 @@ namespace lfsrcrypto {
     }
 
     size_t get_linear_complexity_for_comb_gen(const std::vector<std::string>& regs_gamma,
-            const std::function<size_t(const std::vector<size_t>&)>& func, bool par) {
-        if (par)
-            return get_linear_complexity_for_comb_gen_par(regs_gamma, func);
-        else
-            return get_linear_complexity_for_comb_gen_nonpar(regs_gamma, func);
+            const std::function<size_t(const std::vector<size_t>&)>& func, EXEC_TYPE exec_t) {
+        size_t result;
+        switch(exec_t) {
+            case EXEC_TYPE::SEQ:
+                result = get_linear_complexity_for_comb_gen_seq(regs_gamma, func);
+                break;
+            case EXEC_TYPE::THREADS:
+                result = get_linear_complexity_for_comb_gen_threads(regs_gamma, func);
+                break;
+            case EXEC_TYPE::ASYNC:
+                result = get_linear_complexity_for_comb_gen_async(regs_gamma, func);
+                break;
+            case EXEC_TYPE::OPENMP:
+                result = get_linear_complexity_for_comb_gen_openmp(regs_gamma, func);
+                break;
+        }
+        return result;
     }
 
     std::string gen_rand_gamma(size_t size) {
         static std::random_device rd;
         std::mt19937_64 mt(rd());
-        if (size > 64)
-            size = 64;
-        auto lower_bound = std::pow(2, size/2);
-        auto upper_bound = std::pow(2, size) - 1;
+        size_t block_size = 64;
+        size_t num_of_blocks = size / block_size + (size % block_size > 0 ? 1 : 0);
+        auto lower_bound = std::pow(2, block_size/2);
+        auto upper_bound = std::pow(2, block_size) - 1;
         std::uniform_int_distribution<uint64_t> uniform_dist(lower_bound, upper_bound);
 
         std::string out;
-        boost::to_string(boost::dynamic_bitset<>(size, uniform_dist(mt)), out);
-        if (size > 64) {
+        for (size_t i = 0; i < num_of_blocks; ++i) {
             std::string tmp;
             boost::to_string(boost::dynamic_bitset<>(size, uniform_dist(mt)), tmp);
             out += tmp;
